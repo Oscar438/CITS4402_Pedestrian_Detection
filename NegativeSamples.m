81343487d0fa44c678711d76bf24524ec7004d00
Thomas Vickers <21293921@student.uwa.edu.au>
2018-04-26 13:52:20 +0800

    Negative samples obtained and manipulated. To do: extract hoggs, combine with postives and do svm.

---
+++ NegativeSamples.m
@@ -0,0 +1,25 @@
+baseDir = [pwd '\Negative\'];
+
+files = dir(baseDir); files(1:2) = [];
+
+for ii = 1:length(files)
+    im = imread(files(ii).name);
+    [rows, cols] = size(im);
+    centreX = cols/2;
+    centreY = rows/2;
+    sizeY = rows/4;
+    sizeX = cols/4;
+    imageCropped = imcrop(im, [centreX-(sizeX/2),centreY-(sizeY/2),sizeX,sizeY]);
+    imshow(imageCropped);
+    pause(1);
+    sizeY = rows/3;
+    sizeX = cols/3;
+    imageCropped = imcrop(im, [centreX-(sizeX/2),centreY-(sizeY/2),sizeX,sizeY]);
+    imshow(imageCropped);
+    pause(1);
+    sizeY = rows/2;
+    sizeX = cols/2;
+    imageCropped = imcrop(im, [centreX-(sizeX/2),centreY-(sizeY/2),sizeX,sizeY]);
+    imshow(imageCropped);
+    pause(1);
+end
--- cropping.m
+++ cropping.m
@@ -3,11 +3,12 @@ annotDir = [baseDir 'PennFudanPed\Annotation\'];
 
 files = dir(annotDir); files(1:2) = [];
 close all;
+count = 1;
+test = zeros(335, 324);
 for ii = 1 : 130
     fileName = [annotDir files(ii).name];
     record = PASreadrecord(fileName);
     max = 0;
-    test = zeros(130, 324);
     for jj = 1 : length(record.objects)
         bbox = record.objects(jj).bbox;
         bbox(3:4) = bbox(3:4) - bbox(1:2);
@@ -15,11 +16,12 @@ for ii = 1 : 130
         imageCropped = imcrop(image,bbox);
         imageresized = imresize(imageCropped,[80,20]);
         test2 = hog_feature_vector(imageresized);
-        test(jj,:) = test2;
-        imshow(imageCropped);
+        test(count,:) = test2;
+        count= count+1;
+        %imshow(imageCropped);
     end
-    hold off;    %80x20
 end
 
+for (
 % Next step is to scale all the images to the same size, was it 80x20? I
 % can't remember.
